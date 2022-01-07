3d969adca1ca895f8e0c1ce861321654dc2a9305
Steve Wasiswa <stevewasiswa@gmail.com>
2022-01-07 00:05:00 -0600

    Changed database to SQlite for development

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -81,11 +81,8 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
-       
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
