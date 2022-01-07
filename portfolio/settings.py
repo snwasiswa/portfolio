64304ab7379c1a7a501ca8ab419bcaae8bbd9f3b
snwasiswa <42876758+snwasiswa@users.noreply.github.com>
2022-01-06 23:34:56 -0600

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -84,8 +84,7 @@ DATABASES = {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'portfolio',
-        'PASSWORD': 'portfolio125010!',
+       
 
     }
 }
