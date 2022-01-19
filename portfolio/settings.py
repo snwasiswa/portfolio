600325ab9d2e523ab482cdf82ceb7021d8da0971
Steve Wasiswa <stevewasiswa@gmail.com>
2022-01-18 22:39:03 -0600

    Fixing bugs

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -145,6 +145,20 @@ STATICFILES_DIRS = [
    # os.path.join(BASE_DIR, 'media')
 ]
 
+# EMAIL SETTINGS
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
+
+ADMIN_EMAIL = os.environ.get("ADMIN_EMAIL")
+ADMIN_NAME = os.environ.get("ADMIN_NAME")
+
+ADMINS = os.environ.get("ADMINS")
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
