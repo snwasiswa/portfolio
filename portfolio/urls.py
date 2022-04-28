9c932f01b2df38f3ac7d212daf7a0c19a9a95773
Steve Wasiswa <stevewasiswa@gmail.com>
2022-04-28 12:11:16 -0500

    Fix bugs

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('captcha/', include('captcha.urls')),
+    # path('captcha/', include('captcha.urls')),
 
 ]
 
